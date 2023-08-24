bd5f178b1bf7dd80c93bfd5ce39a80a9c47c91f9
Harsh Patel <Patelharsh241999@gmail.com>
2023-08-25 01:12:11 +0530

    * Adding Caching in fetchdatamethod so it gives performace boost and stores data in localstorage . .

--- src/services/spaceXService.ts
+++ src/services/spaceXService.ts
@@ -1,17 +1,39 @@
 import { Capsule } from "../configs/types/Types";
 
-export const fetchSpaceXData = async (filters:string):Promise<Capsule[]> => {
-    const queryString = new URLSearchParams(filters).toString();
- return await fetch(`https://api.spacexdata.com/v3/capsules?${queryString}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(async (e) => {
-      const response = await e.json();
-      console.log("response : ", response);
-      return response;
+const LOCAL_STORAGE_KEY = "localstoredspacexdata";
+const dataCache: Record<string, Capsule[]> = JSON.parse(
+  localStorage.getItem(LOCAL_STORAGE_KEY) || "{}"
+);
+
+export const fetchSpaceXData = async (
+  filters: Record<string, string>
+): Promise<Capsule[]> => {
+  const filtersString: string = JSON.stringify(filters);
+
+  if (dataCache[filtersString]) {
+    return dataCache[filtersString];
+  }
+
+  const queryString = new URLSearchParams(filters).toString();
+  const fetchedData = await fetch(
+    `https://api.spacexdata.com/v3/capsules?${queryString}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then(async (response) => {
+      const data = await response.json();
+      return data;
     })
-    .catch(() => {return []});
+    .catch(() => []);
+
+  dataCache[filtersString] = fetchedData;
+
+  // Store the updated dataCache in local storage
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataCache));
+
+  return fetchedData;
 };
